155e59a7e3c7b07460ff06ae1aabbc4dfbd122b2
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-06-23 13:12:21 +0600

    create public folder structure with group layout

---
+++ src/app/(public)/all-tickets/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const allTicketPage = () => {
+    return (
+        <div>
+           Show all tickets here 
+        </div>
+    );
+};
+
+export default allTicketPage;
---
+++ src/app/(public)/layout.jsx
@@ -0,0 +1,14 @@
+export default function PublicLayout({ children }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+    
+      {/* <Navbar /> */}
+      
+      <main className="grow container mx-auto px-4 py-8">
+        {children}
+      </main>
+
+      {/* <Footer /> */}
+    </div>
+  );
+}
