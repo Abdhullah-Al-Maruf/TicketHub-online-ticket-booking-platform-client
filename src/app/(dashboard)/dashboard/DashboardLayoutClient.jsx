2b1dcdbfa5c6a38b94722d1338b6970f694a66e4
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-06-28 12:42:42 +0600

    fixed navbar background and glassmorphism effect in light and dark mode

--- src/app/(dashboard)/dashboard/DashboardLayoutClient.jsx
+++ src/app/(dashboard)/dashboard/DashboardLayoutClient.jsx
@@ -11,7 +11,7 @@ export default function DashboardLayoutClient({ children, user }) {
     <div className="min-h-screen flex flex-col  text-[var(--on-surface)]">
       
       {/* Standalone Hamburger Trigger (Only visible on mobile/tablet screens) */}
-      <div className="lg:hidden flex items-center p-4 border-b border-[var(--outline-variant)]  sticky top-0 z-40">
+      <div className="lg:hidden flex items-center p-4 border-b border-[var(--outline-variant)] bg-white/50 backdrop:blur-in-md dark:bg-black/50 backdrop:backdrop-blur-lg  sticky top-0 z-40">
         <button
           onClick={() => setIsMobileOpen(true)}
           className="p-2 text-[var(--outline)] hover:text-[var(--on-surface)] hover:bg-[var(--surface-container-high)] rounded-md transition-all"
